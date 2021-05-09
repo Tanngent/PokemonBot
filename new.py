ff2e8253662d20e79ea4c9d6e5d4e21184a7cbed
Tanngent <maxtang77@gmail.com>
2021-05-09 17:37:47 -0400

    Update new.py

--- new.py
+++ new.py
@@ -3,10 +3,13 @@ import websockets
 import requests
 
 async def hello():
-    uri = "ws://sim.smogon.com:8000/showdown/websocket"
-    async with websockets.connect(uri) as websocket:
-        while True:
-            greeting = await websocket.recv()
-            print(f"< {greeting}")
+	uri = "ws://sim.smogon.com:8000/showdown/websocket"
+	async with websockets.connect(uri) as websocket:
+		greeting = await websocket.recv();
+		print(f"< {greeting}")
+		while True:
+			greeting = await websocket.recv()
+			print(f"< {greeting}")
+			url = "https://play.pokemonshowdown.com/~~showdown/action.php"
 
 asyncio.get_event_loop().run_until_complete(hello())
