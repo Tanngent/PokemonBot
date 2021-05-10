26a914b9f1918102e18c42d4c8dea24e8ff6efe3
Tanngent <maxtang77@gmail.com>
2021-05-09 23:21:45 -0400

    Update new.py

--- new.py
+++ new.py
@@ -1,6 +1,7 @@
 import asyncio
 import websockets
 import requests
+import json
 
 async def hello():
 	uri = "ws://sim.smogon.com:8000/showdown/websocket"
@@ -14,5 +15,19 @@ async def hello():
 		print(f"< {greeting}")
 		greeting = await websocket.recv();
 		print(f"< {greeting}")
+		header = {'Content-type': 'application/x-www-form-urlencoded; encoding=UTF-8'}
+		content = {'act': 'login', 'name': '0hzlf9CCSL','pass': '123456', 'challstr': greeting[10:]}
+		print(content)
+		r = requests.post(url,data=content,headers=header)
+		print(r.text[1:])
+		s = json.loads(r.text[1:])
+		print(s)
+		l = "|/trn 0hzlf9CCSL,0," + s['assertion']
+		print(l)
+		await websocket.send(l)
+		while True:
+			greeting = await websocket.recv();
+			print(f"< {greeting}")
+		
 
 asyncio.get_event_loop().run_until_complete(hello())
