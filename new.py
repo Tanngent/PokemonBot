6311806ca6ee0644aedbcb32b13a9161b6eb074a
Tanngent <maxtang77@gmail.com>
2021-07-08 21:16:46 -0400

    Update new.py

--- new.py
+++ new.py
@@ -3,31 +3,38 @@ import websockets
 import requests
 import json
 
-async def hello():
-	uri = "ws://sim.smogon.com:8000/showdown/websocket"
+async def inp(queue, websocket):
 	url = "https://play.pokemonshowdown.com/~~showdown/action.php"
-	async with websockets.connect(uri) as websocket:
+	greeting = await websocket.recv();
+	print(f"< {greeting}")
+	while not greeting.startswith("|challstr|"):
 		greeting = await websocket.recv();
 		print(f"< {greeting}")
-		greeting = await websocket.recv();
+	f = open("user.txt","r")
+	usname = f.readline()
+	passwd = f.readline()
+	header = {'Content-type': 'application/x-www-form-urlencoded; encoding=UTF-8'}
+	content = {'act': 'login', 'name': usname[:-1],'pass': passwd, 'challstr': greeting[10:]}
+	r = requests.post(url,data=content,headers=header)
+	s = json.loads(r.text[1:])
+	l = "|/trn " + usname[0:-1] +",0," + s['assertion']
+	await queue.put(l)
+	while True:
+		greeting = await websocket.recv()
 		print(f"< {greeting}")
-		greeting = await websocket.recv();
-		print(f"< {greeting}")
-		greeting = await websocket.recv();
-		print(f"< {greeting}")
-		header = {'Content-type': 'application/x-www-form-urlencoded; encoding=UTF-8'}
-		content = {'act': 'login', 'name': '0hzlf9CCSL','pass': '123456', 'challstr': greeting[10:]}
-		print(content)
-		r = requests.post(url,data=content,headers=header)
-		print(r.text[1:])
-		s = json.loads(r.text[1:])
-		print(s)
-		l = "|/trn 0hzlf9CCSL,0," + s['assertion']
-		print(l)
-		await websocket.send(l)
-		while True:
-			greeting = await websocket.recv();
-			print(f"< {greeting}")
-		
+			
+async def out(queue, websocket):
+	while True:
+		token = await queue.get()
+		await websocket.send(token)
+		print(f"> {token}")
+		queue.task_done()
+	
+async def main():
+	queue = asyncio.Queue()
+	uri = "ws://sim.smogon.com:8000/showdown/websocket"
+	async with websockets.connect(uri) as websocket:	
+		await asyncio.gather(asyncio.create_task(inp(queue, websocket)),asyncio.create_task(out(queue, websocket)))
 
-asyncio.get_event_loop().run_until_complete(hello())
+
+asyncio.run(main())
