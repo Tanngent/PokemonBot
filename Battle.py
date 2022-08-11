7ccd6c153efc89066a06035d5b192ab63e09c8d3
Tanngent <maxtang77@gmail.com>
2022-08-11 00:19:29 -0400

    Now able to play games
    Also changed to single quotes

--- Battle.py
+++ Battle.py
@@ -1,12 +1,12 @@
+import random
+
+
 class Pokemon:
     def __init__(self):
-        self.specie = 'null'
-        self.level = 100
+        self.specie = ''
+        self.level = 0
         self.health = 100
-        self.move1 = 'null'
-        self.move2 = 'null'
-        self.move3 = 'null'
-        self.move4 = 'null'
+        self.moves = ['','','','']
         self.atk = 0
         self.defe = 0
         self.spa = 0
@@ -14,41 +14,87 @@ class Pokemon:
         self.spe = 0
 
     def update(self,json):
-        print(json)
-        self.specie = json['details']
-        self.level = json['details']
-        self.health = json['details']
-        self.move1 = json['moves'][0]
-        self.move2 = json['moves'][1]
-        self.move3 = json['moves'][2]
-        self.move4 = json['moves'][3]
+        #print(json)
+        self.specie = json['details'].split(' ')[0].translate(str.maketrans('', '', ', '))
+        self.level = 100 if len(json['details'].split(' ')) == 1 else json['details'].split(' ')[1].translate(str.maketrans('', '', 'L, '))
+        self.health = 0.0 if json['condition'].split(' ')[0] == '0' else int(json['condition'].split(' ')[0].split('/')[0])/int(json['condition'].split(' ')[0].split('/')[1])
+        self.condition = '' if len(json['condition'].split(' ')) == 1 else json['condition'].split(' ')[1]
+        self.moves[0] = json['moves'][0]
+        self.moves[1] = '' if len(json['moves']) <= 1 else json['moves'][1]
+        self.moves[2] = '' if len(json['moves']) <= 2 else json['moves'][2]
+        self.moves[3] = '' if len(json['moves']) <= 3 else json['moves'][3]
         self.atk = json['stats']['atk']
         self.defe = json['stats']['def']
         self.spa = json['stats']['spa']
         self.spd = json['stats']['spd']
         self.spe = json['stats']['spe']
+    
+    def __str__(self):
+        return str(self.specie) + ', ' + str(self.level) + ', ' + str(self.health) + ', ' + self.condition + ', ' + self.moves[0]\
+            + ', ' + self.moves[1] + ', ' + self.moves[2] + ', ' + self.moves[3] + ', ' + str(self.atk) + ', ' + str(self.defe)\
+                 + ', ' + str(self.spa)  + ', ' + str(self.spd)  + ', ' + str(self.spe)
+
+
 
 class Battle:
     def __init__(self):
         self.active = 1
-        self.pokemon1 = Pokemon()
-        self.pokemon2 = Pokemon()
-        self.pokemon3 = Pokemon()
-        self.pokemon4 = Pokemon()
-        self.pokemon5 = Pokemon()
-        self.pokemon6 = Pokemon()
+        self.ownTeam = [Pokemon(), Pokemon(), Pokemon(), Pokemon(), Pokemon(), Pokemon()]
+        self.enemyTeam = [Pokemon(), Pokemon(), Pokemon(), Pokemon(), Pokemon(), Pokemon()]
+        self.forceSwitch = False
+        self.trapped = False
+        self.disabled = [False, False, False, False]
 
-    def update(self,json):
-        print(json)
-        print(json['side'])
-        print(json['side']['pokemon'])
+    def updateSelf(self,json):
+        #print(json)
+        #print(json['side'])
+        #print(json['side']['pokemon'])
+
         i = 0
         while(not json['side']['pokemon'][i]['active']):
             i += 1
         self.active = i
-        self.pokemon1.update(json['side']['pokemon'][0])
-        self.pokemon2.update(json['side']['pokemon'][1])
-        self.pokemon3.update(json['side']['pokemon'][2])
-        self.pokemon4.update(json['side']['pokemon'][3])
-        self.pokemon5.update(json['side']['pokemon'][4])
-        self.pokemon6.update(json['side']['pokemon'][5])
+
+        self.ownTeam[0].update(json['side']['pokemon'][0])
+        self.ownTeam[1].update(json['side']['pokemon'][1])
+        self.ownTeam[2].update(json['side']['pokemon'][2])
+        self.ownTeam[3].update(json['side']['pokemon'][3])
+        self.ownTeam[4].update(json['side']['pokemon'][4])
+        self.ownTeam[5].update(json['side']['pokemon'][5])
+        self.forceSwitch = 'forceSwitch' in json
+        self.trapped = False
+        if('active' in json):
+            self.disabled[0] = False if 'disabled' in json['active'][0]['moves'][0] else json['active'][0]['moves'][0]['disabled']
+            self.disabled[1] = True if len(json['active'][0]['moves']) <= 1 else json['active'][0]['moves'][1]['disabled']
+            self.disabled[2] = True if len(json['active'][0]['moves']) <= 2 else json['active'][0]['moves'][2]['disabled']
+            self.disabled[3] = True if len(json['active'][0]['moves']) <= 3 else json['active'][0]['moves'][3]['disabled']
+            self.trapped = 'trapped' in json['active'][0]
+
+    def getMove(self):
+        validMoves = []
+        validSwitches = []
+        for i in range(6):
+            if i != self.active and self.ownTeam[i].condition != 'fnt':
+                validSwitches.append('switch ' + str(i + 1))
+        switchChoice = -1 if len(validSwitches) == 0 else random.randint(0, len(validSwitches) - 1)
+        print(validSwitches)
+        print(switchChoice)
+        for i in range(4):
+            if not self.disabled[i]:
+                validMoves.append('move ' + str(i + 1))
+        moveOrSwitch = random.randint(1, 10)
+        moveChoice = random.randint(0, len(validMoves) - 1)
+        print(validMoves)
+        print(moveChoice)
+        print(moveOrSwitch)
+        if not self.trapped and switchChoice != -1 and (self.forceSwitch or moveOrSwitch > 6):
+            return validSwitches[switchChoice]
+        else:
+            return validMoves[moveChoice]
+        
+
+
+    def __str__(self):
+        return self.ownTeam[0].__str__() + '::\n' + self.ownTeam[1].__str__() + '::\n' + self.ownTeam[2].__str__() + '::\n' + self.ownTeam[3].__str__()\
+             + '::\n' + self.ownTeam[4].__str__() + '::\n' + self.ownTeam[5].__str__() + '::\n' + str(self.active) + '::\n' + str(self.forceSwitch)\
+                + '::\n' + str(self.disabled[0]) + '::\n' + str(self.disabled[1]) + '::\n' + str(self.disabled[2]) + '::\n' + str(self.disabled[3])
--- IOLayer.py
+++ IOLayer.py
@@ -6,26 +6,26 @@ import json
 from Battle import Battle
 
 async def inp(queue, websocket):
-	url = "https://play.pokemonshowdown.com/~~showdown/action.php"
+	url = 'https://play.pokemonshowdown.com/~~showdown/action.php'
 	greeting = await websocket.recv()
-	#print(f"<<< {greeting}")
-	while not greeting.startswith("|challstr|"):
+	#print(f'<<< {greeting}')
+	while not greeting.startswith('|challstr|'):
 		greeting = await websocket.recv()
-		#print(f"<<< {greeting}")
-	f = open("user.txt","r")
+		#print(f'<<< {greeting}')
+	f = open('user.txt','r')
 	usname = f.readline()
 	passwd = f.readline()
 	header = {'Content-type': 'application/x-www-form-urlencoded; encoding=UTF-8'}
 	content = {'act': 'login', 'name': usname[:-1],'pass': passwd, 'challstr': greeting[10:]}
 	r = requests.post(url,data=content,headers=header)
 	s = json.loads(r.text[1:])
-	l = "|/trn " + usname[0:-1] +",0," + s['assertion']
+	l = '|/trn ' + usname[0:-1] +',0,' + s['assertion']
 	await queue.put(l)
 	games = {}
 	while True:
 		greeting = await websocket.recv()
-		print(f"<<< {greeting}")
-		if greeting.startswith("|updatesearch|"):
+		print(f'<<< {greeting}')
+		if greeting.startswith('|updatesearch|'):
 			s = json.loads(greeting[14:])
 			localgames = list(games)
 			remotegames = None
@@ -39,65 +39,64 @@ async def inp(queue, websocket):
 				battlequeue = asyncio.Queue()
 				asyncio.gather(asyncio.create_task(battle(battlequeue,queue,game)))
 				games[game] = battlequeue
-				await queue.put(f"|/join {game}")
+				await queue.put(f'|/join {game}')
 			for game in endedgames:
-				await games.get(game).put("end")
+				await games.get(game).put('end')
 				games.pop(game,None)
-				await queue.put(f"|/leave {game}")
-		elif greeting.startswith("|pm|"):
-			bits = greeting.split("|")
+				await queue.put(f'|/leave {game}')
+		elif greeting.startswith('|pm|'):
+			bits = greeting.split('|')
 			other = bits[2].strip()
-			if bits[4].startswith("/challenge"):
-				await queue.put(f"|/accept {other}")
-		elif greeting.startswith(">battle"):
+			if bits[4].startswith('/challenge'):
+				await queue.put(f'|/accept {other}')
+		elif greeting.startswith('>battle'):
 			gamename = greeting.partition('\n')[0][1:]
 			if gamename in games:
 				await games[gamename].put(greeting)
 			else:
-				print(f"Battle with name {gamename} does not exist or has already ended\n")
+				print(f'Battle with name {gamename} does not exist or has already ended\n')
 
 async def out(queue, websocket):
 	while True:
 		token = await queue.get()
 		await websocket.send(token)
-		print(f">>> {token}")
+		print(f'>>> {token}')
 		queue.task_done()
 
 async def battle(queuein, queueout, str):
-	print(f"Started {str}")
+	print(f'Started {str}')
 	thisBattle = Battle()
 	receivedRequest = False
 	receivedChange = False
 	while True:
 		if queuein.empty():
-			#
-			#await queueout.put(f"{str}|/choose move 1")
+			#await queueout.put(f'{str}|/choose move 1')
 			await asyncio.sleep(1)
 		else:
 			token = await queuein.get()
-			if token == "end":
+			if token == 'end':
 				return
 			else:
-				#print(f"<<< {token}")
-				lines = token.split("\n")[1:]
-				if lines[0].startswith("|request|") and lines[0] != "|request|":
-					#print("receivedRequest")
+				#print(f'<<< {token}')
+				lines = token.split('\n')[1:]
+				if lines[0].startswith('|request|') and lines[0] != '|request|':
+					#print('receivedRequest')
 					#print(lines[0][9:])
 					teaminfo = json.loads(lines[0][9:])
-					thisBattle.update(teaminfo)
+					thisBattle.updateSelf(teaminfo)
 					print(thisBattle)
 					receivedRequest = True
-				elif lines[0] == "|" or "|start" in lines:
-					#print("receivedChange")
+				elif lines[0] == '|' or '|start' in lines:
+					#print('receivedChange')
 					receivedChange = True
 				if receivedRequest and receivedChange:
-					await queueout.put(f"{str}|/choose move 1")
+					await queueout.put(f'{str}|/choose {thisBattle.getMove()}')
 					receivedRequest = False
 					receivedChange = False
 
 async def main():
 	queue = asyncio.Queue()
-	uri = "ws://sim.smogon.com:8000/showdown/websocket"
+	uri = 'ws://sim.smogon.com:8000/showdown/websocket'
 	async with websockets.connect(uri) as websocket:
 		await asyncio.gather(asyncio.create_task(inp(queue, websocket)),asyncio.create_task(out(queue, websocket)))
 
