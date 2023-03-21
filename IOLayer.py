fabab3c7fcab22327e4753379ae11c61530ec067
Tanngent <maxtang77@gmail.com>
2023-03-21 18:47:56 -0400

    Added some comments

--- IOLayer.py
+++ IOLayer.py
@@ -5,100 +5,103 @@ import json
 
 from Battle import Battle
 
-async def inp(queue, websocket):
+async def inp(queue, websocket): # accept incoming websocket message
 	url = 'https://play.pokemonshowdown.com/~~showdown/action.php'
 	greeting = await websocket.recv()
-	#print(f'<<< {greeting}')
+	# print(f'<<< {greeting}')
+	# wait for challstr
 	while not greeting.startswith('|challstr|'):
 		greeting = await websocket.recv()
-		#print(f'<<< {greeting}')
+		# print(f'<<< {greeting}')
 	f = open('user.txt','r')
 	usname = f.readline()
 	passwd = f.readline()
 	header = {'Content-type': 'application/x-www-form-urlencoded; encoding=UTF-8'}
 	content = {'act': 'login', 'name': usname[:-1],'pass': passwd, 'challstr': greeting[10:]}
+	# send the login request
 	r = requests.post(url,data=content,headers=header)
 	s = json.loads(r.text[1:])
 	l = '|/trn ' + usname[0:-1] +',0,' + s['assertion']
+	# put the assertion 
 	await queue.put(l)
 	games = {}
 	while True:
 		greeting = await websocket.recv()
 		print(f'<<< {greeting}')
-		if greeting.startswith('|updatesearch|'):
+		if greeting.startswith('|updatesearch|'): # new game started or game stopped
 			s = json.loads(greeting[14:])
 			localgames = list(games)
 			remotegames = None
-			if s['games']:
+			if s['games']: # this will exist if there is a game going on
 				remotegames = list(s['games'])
 			else:
 				remotegames = []
 			startedgames = set(remotegames) - set(localgames)
 			endedgames = set(localgames) - set(remotegames)
-			for game in startedgames:
+			for game in startedgames: # games that just started
 				battlequeue = asyncio.Queue()
 				asyncio.gather(asyncio.create_task(battle(battlequeue,queue,game,usname)))
 				games[game] = battlequeue
 				await queue.put(f'|/join {game}')
-			for game in endedgames:
+			for game in endedgames: # games taht just ended
 				await games.get(game).put('end')
 				games.pop(game,None)
 				await queue.put(f'|/leave {game}')
 			#if len(games) == 0:
-				#await queue.put('|/challenge 0hzlf9ccsl, gen1randombattle')
-		elif greeting.startswith('|pm|'):
+				# await queue.put('|/challenge 0hzlf9ccsl, gen1randombattle')
+		elif greeting.startswith('|pm|'): # pm, including challenge request
 			bits = greeting.split('|')
 			other = bits[2].strip()
-			if bits[4].startswith('/challenge'):
+			if bits[4].startswith('/challenge'): # accept challenges
 				await queue.put(f'|/accept {other}')
-		elif greeting.startswith('>battle'):
+		elif greeting.startswith('>battle'): # message to a specific battle
 			gamename = greeting.partition('\n')[0][1:]
 			if gamename in games:
 				await games[gamename].put(greeting)
 			else:
 				print(f'Battle with name {gamename} does not exist or has already ended\n')
 
-async def out(queue, websocket):
+async def out(queue, websocket): # send websocket message
 	while True:
 		token = await queue.get()
 		await websocket.send(token)
 		print(f'>>> {token}')
 		queue.task_done()
 
-async def battle(queuein, queueout, str, usname):
+async def battle(queuein, queueout, str, usname): # battle object for handling messaging
 	print(f'Started {str}')
 	thisBattle = Battle()
 	receivedRequest = False
 	receivedChange = False
 	while True:
 		if queuein.empty():
-			#await queueout.put(f'{str}|/choose move 1')
+			# await queueout.put(f'{str}|/choose move 1')
 			await asyncio.sleep(1)
 		else:
 			token = await queuein.get()
-			if token == 'end':
+			if token == 'end': # end battle
 				thisBattle.endBattle()
 				return
 			else:
-				#print(f'<<< {token}')
+				# print(f'<<< {token}')
 				lines = token.split('\n')[1:]
-				if lines[0].startswith('|request|') and lines[0] != '|request|':
-					#print('receivedRequest')
-					#print(lines[0][9:])
+				if lines[0].startswith('|request|') and lines[0] != '|request|': # request for move
+					# print('receivedRequest')
+					# print(lines[0][9:])
 					teaminfo = json.loads(lines[0][9:])
 					thisBattle.updateSelf(teaminfo)
 					receivedRequest = True
-				elif lines[0] == '|' or '|start' in lines:
-					#print('receivedChange')
+				elif lines[0] == '|' or '|start' in lines: # battle update
+					# print('receivedChange')
 					thisBattle.parseChange(lines, usname.strip())
 					receivedChange = True
-				if receivedRequest and receivedChange:
+				if receivedRequest and receivedChange: # received both, make decision
 					print(thisBattle)
 					await queueout.put(f'{str}|/choose {thisBattle.getMove()}')
 					receivedRequest = False
 					receivedChange = False
 
-async def main():
+async def main(): # program main
 	queue = asyncio.Queue()
 	uri = 'ws://sim.smogon.com:8000/showdown/websocket'
 	async with websockets.connect(uri) as websocket:
